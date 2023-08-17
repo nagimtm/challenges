1b0bbfbfdc1710970631a53a87dfd51cc1f78eca
nagim <a.nagim@protonmail.com>
2023-08-17 11:16:10 +0200

    small social media post challeng created in the index.js file

--- javascript/js-variables-and-numbers_social-media-post/js/index.js
+++ javascript/js-variables-and-numbers_social-media-post/js/index.js
@@ -10,13 +10,24 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+const title = "Weekend Beer Party";
+const text = "Beer Party is starting on Friday at 17:00 during the bootcamp";
+let numberOfLikes = 0;
+const userName = "Super User";
+const isReported = false;
 // --^-- write your code here --^--
 
 /*
-2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your 
+code from 1.1 if necessary.
 */
 
 // --v-- write your code here --v--
-
+console.log(title);
+numberOfLikes += 1;
+console.log(text);
+numberOfLikes += 1;
+console.log(numberOfLikes);
+console.log(userName);
+console.log(isReported);
 // --^-- write your code here --^--
