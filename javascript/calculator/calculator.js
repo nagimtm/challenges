f82c4b5ff65861261804cd9b3daef353d00c975a
nagim <a.nagim@protonmail.com>
2023-08-17 12:06:05 +0200

    add:  calculator.js file

---
+++ javascript/calculator/calculator.js
@@ -0,0 +1,18 @@
+// Calculator App
+
+// Defining and assigning meal amount.
+let mealAmount = 82.0;
+
+// Defining and assigning tip percentage.
+let tipPercentage = 5;
+
+// Defining and assigning tip amount.
+let tipAmount = (mealAmount / 100) * tipPercentage;
+
+//calculating total cost.
+let totalCost = mealAmount + tipAmount;
+
+const text = "The total cost of your bill is: ";
+const currency = " euros.";
+
+console.log(text + totalCost + currency);
