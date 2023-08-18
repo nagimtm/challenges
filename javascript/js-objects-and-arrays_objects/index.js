5441996ba581ad6113941d1769aad37c958bda29
nagim <a.nagim@protonmail.com>
2023-08-18 16:10:44 +0200

    js-objects challenge done

--- javascript/js-objects-and-arrays_objects/index.js
+++ javascript/js-objects-and-arrays_objects/index.js
@@ -4,8 +4,12 @@ console.clear();
 // Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "Max",
+  age: 19,
+  email: "max19@maxmail.com",
 };
+
+console.log(personWithNameAgeEmail);
 
 // EXERCISE 2
 // Set the values of name and age equal to the corresponding values of the personAlex object.
@@ -15,17 +19,23 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
 
+console.log(nameOfAlex, ageOfAlex);
 // EXERCISE 3
-// Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
+// Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange:
+//person.existingProperty = newValue.
 
 const personToChange = {
   name: "Kim",
   age: 24,
 };
 
+personToChange.name = nameOfAlex;
+personToChange.age = 35;
+
+console.log(personToChange);
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
 
@@ -33,6 +43,13 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+
+console.log(petPluto);
+
+petPluto.age = 5;
+petPluto.breed = "husky";
+
+console.log(petPluto);
 
 export {
   personWithNameAgeEmail,
