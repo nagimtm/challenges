5fde5602667d1b501c352dd5f93144735043d74b
nagim <a.nagim@protonmail.com>
2023-08-21 17:24:13 +0200

    arrow function challenge done

--- javascript/js-functions-2_arrow-functions/js/index.js
+++ javascript/js-functions-2_arrow-functions/js/index.js
@@ -5,7 +5,21 @@ Rewrite the following functions as arrow functions.
 Use implicit returns when possible
 */
 
-function getCurrentHour() {
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }
+// }
+
+const getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -17,21 +31,39 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
 
-function getVectorLength(x, y, z) {
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
+
+const getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+};
 
-function cleanInput(string) {
-  return string.toLowerCase().trim();
-}
+// function cleanInput(string) {
+//   return string.toLowerCase().trim();
+// }
+
+const cleanInput = (string) => string.toLowerCase().trim();
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+// const isOddAndSmall = (number) => {
+//   if (number > 10) {
+//     return false;
+//   }
+
+//   if (number % 2 === 0) {
+//     return false;
+//   }
+
+//   return true;
+// };
+
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -41,8 +73,16 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
 
-const add3 = (a, b, c) => a + b + c;
+// const add3 = (a, b, c) => a + b + c;
 
-const repeat10 = (string) => string.repeat(10);
+function add3(a, b, c) {
+  returna + b + c;
+}
+
+// const repeat10 = (string) => string.repeat(10);
+
+function repeat10(string) {
+  return string.repeat(10);
+}
