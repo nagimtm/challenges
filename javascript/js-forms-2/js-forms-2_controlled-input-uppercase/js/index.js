7b9cb6f8b4dbb4d97930579b531999bb06f8ae08
nagim <a.nagim@protonmail.com>
2023-08-24 11:10:30 +0200

    edited index.js file

--- javascript/js-forms-2/js-forms-2_controlled-input-uppercase/js/index.js
+++ javascript/js-forms-2/js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+input.addEventListener("input", (event) => {
+  const inputValue = event.target.value.toUpperCase();
+  return (input.value = inputValue);
+});
