50bb475e66d7bf81cf3e712a6baa5b5f2aada59b
nagim <a.nagim@protonmail.com>
2023-08-24 16:54:50 +0200

    edited index.js file

--- javascript/js-createElement-challenges/js-createelement_product-teaser/js/index.js
+++ javascript/js-createElement-challenges/js-createelement_product-teaser/js/index.js
@@ -1,7 +1,5 @@
 console.clear();
 
-const body = document.querySelector("body");
-
 const name = "Queen angelfish";
 const description =
   "With their bright colors and deep, laterally compressed bodies, marine angelfishes are some of the more conspicuous residents of the aquarium. The queen angelfish grows to be 45 cm. With neon blue and yellow scales and iridescent purple and orange markings, surprisingly it is not conspicuous, and actually hides very well, and is very shy.";
@@ -22,23 +20,28 @@ const productTextContainer = document.createElement("div");
 productTextContainer.classList.add("product__text-container");
 
 const productName = document.createElement("h2");
-productName.textContent = "Queen angelfish";
+productName.textContent = "Queen Angelfish";
 
 // const category1 = "Freshwater";
 // const category2 = "Large aquarium";
 // const category3 = "Plankton Diet";
 const productCategories = document.createElement("ul");
+
 productCategories.classList.add("product__categories");
-const productCategory_1 = docucment.createElement("li");
-productCategory_1.classList.add("prodact_category");
+
+const productCategory_1 = document.createElement("li");
+
+productCategory_1.classList.add("product__category");
 productCategory_1.textContent = "Freshwater";
 
-const productCategory_2 = docucment.createElement("li");
-productCategory_2.classList.add("prodact_category");
-productCategory_2.textContent = "Large aquarium";
+const productCategory_2 = document.createElement("li");
 
-const productCategory_3 = docucment.createElement("li");
-productCategory_3.classList.add("prodact_category");
+productCategory_2.classList.add("product__category");
+productCategory_2.textContent = "Large Aquarium";
+
+const productCategory_3 = document.createElement("li");
+
+productCategory_3.classList.add("product__category");
 productCategory_3.textContent = "Plankton Diet";
 
 productCategories.append(
@@ -58,6 +61,7 @@ productDescription.textContent = `With their bright colors and deep, laterally c
 productTextContainer.append(productName, productCategories, productDescription);
 
 const productImageContaner = document.createElement("div");
+
 productImageContaner.classList.add("product__image-container");
 
 const productImg = document.createElement("img");
@@ -66,10 +70,24 @@ productImg.setAttribute(
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640"
 );
 productImg.classList.add("product__image");
-
 productImageContaner.append(productImg);
-productBody.append(productTextContainer);
 
-newProduct.append(productBody);
+productBody.append(productTextContainer, productImageContaner);
 
-// body.append(newProduct);
+const productFooter = document.createElement("footer");
+productFooter.classList.add("product__footer");
+
+const productPrice = document.createElement("span");
+productPrice.classList.add("product__price");
+productPrice.textContent = "149,99 €";
+
+const buyButton = document.createElement("button");
+buyButton.classList.add("product__buy-button");
+buyButton.setAttribute("type", "submit");
+buyButton.textContent = "Buy";
+
+productFooter.append(productPrice, buyButton);
+newProduct.append(productBody, productFooter);
+
+const body = document.querySelector("body");
+body.append(newProduct);
