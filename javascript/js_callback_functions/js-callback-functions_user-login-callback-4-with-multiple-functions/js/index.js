097cbb6932ef7ffe445e6461dda51669f35e79dd
nagim <a.nagim@protonmail.com>
2023-08-29 11:42:04 +0200

    added and edited callback function with three params in index.js

--- javascript/js_callback_functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
+++ javascript/js_callback_functions/js-callback-functions_user-login-callback-4-with-multiple-functions/js/index.js
@@ -9,3 +9,19 @@ function handleUserLogin(onSuccess, onError, userName) {
 }
 
 // The exercise starts here!
+
+// create showWelcomeMessage
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+// create showErrorMessage
+function showErrorMessage(errorMessage) {
+  console.log(`Login error: ${errorMessage}`);
+}
+
+//calling handleUserLogin
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jane Doe");
+
+// calling handleUseerLogin different userName;
+handleUserLogin(showWelcomeMessage, showErrorMessage, "Jahn Doe");
