0400a14139ed34d7cc32d1f81deba599a4ffb820
nagim <a.nagim@protonmail.com>
2023-08-29 11:19:55 +0200

    added and edited index.js file two functions, callback and anonymous with argument.

--- javascript/js_callback_functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ javascript/js_callback_functions/js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,16 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+// creating whowWelcomeMessage callback function
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+}
+
+// calling handleUserLogin functoin with callback function and userName
+handleUserLogin(showWelcomeMessage);
+
+//calling handleUserLogin function with anonymous function
+handleUserLogin((userName) => {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+});
