eb92860cdbe3b60a08dcaa04be428e3e4fd8a1ef
nagim <a.nagim@protonmail.com>
2023-08-30 16:03:30 +0200

    Add: import function from util and use it styling the elements

--- javascript/js-structure/js-structure_shapes/index.js
+++ javascript/js-structure/js-structure_shapes/index.js
@@ -1,3 +1,5 @@
+// importing getRandomColor function form utils folder
+import { getRandomColor } from "./utils/randomColor.js";
 console.clear();
 
 const root = document.getElementById("root");
@@ -5,19 +7,22 @@ const root = document.getElementById("root");
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
+  // defining gerRandom color to circle's style
+  circle.style.backgroundColor = getRandomColor();
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  // defining gerRandom color to square's style
+  square.style.backgroundColor = getRandomColor();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  // defining gerRandom color to pentagon's style
+  pentagon.style.backgroundColor = getRandomColor();
 });
 
 root.append(circle, square, pentagon);
