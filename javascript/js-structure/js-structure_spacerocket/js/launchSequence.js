f5d45dd2595566e7dfc891282eb9bc4fe2c0ed77
nagim <a.nagim@protonmail.com>
2023-09-02 18:48:27 +0200

    required sequance of space rocket done

--- javascript/js-structure/js-structure_spacerocket/js/launchSequence.js
+++ javascript/js-structure/js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,22 @@
+import { loadPayload } from "./core/load.js";
+import { fuel } from "./core/fuel.js";
+import { countdown } from "./core/countdown.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
+import { NFSAT, FISHSAT } from "./payload/satellites.js";
 // Implement the launch sequence function here and export it as the default export.
+
+export default function launchSequence() {
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+  fuel();
+
+  const func = () => countdown();
+  const times = 5;
+
+  Array.from({ length: times }, () => func());
+
+  liftoff();
+
+  deployPayload();
+}
