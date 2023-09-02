9f005e00d74e38a0c3db156340f1e074b41c1081
nagim <a.nagim@protonmail.com>
2023-09-02 19:47:02 +0200

    Added sequence functions space rocket

--- javascript/js-structure/js-structure_spacerocket/js/launchSequence.js
+++ javascript/js-structure/js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,19 @@
 // Implement the launch sequence function here and export it as the default export.
+import { loadPayload } from "./core/load.js";
+import { fuel } from "./core/fuel.js";
+import { countdown } from "./core/countdown.js";
+import { liftoff } from "./core/liftoff.js";
+import { deployPayload } from "./core/deploy.js";
+import { NFSAT, FISHSAT } from "./payload/satellites.js";
+
+export default function launchSequence() {
+  loadPayload(NFSAT);
+  loadPayload(FISHSAT);
+  console.log("hi");
+  fuel();
+  const fn = () => countdown();
+  const times = 5;
+  Array.from({ length: times }, () => fn());
+  liftoff();
+  deployPayload();
+}
