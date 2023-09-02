b8309ce096cba3f3f07af16f107c1b34c9e98dff
nagim <a.nagim@protonmail.com>
2023-09-02 18:54:31 +0200

    last changes added

--- javascript/js-structure/js-structure_spacerocket/js/launchSequence.js
+++ javascript/js-structure/js-structure_spacerocket/js/launchSequence.js
@@ -5,7 +5,6 @@ import { liftoff } from "./core/liftoff.js";
 import { deployPayload } from "./core/deploy.js";
 import { NFSAT, FISHSAT } from "./payload/satellites.js";
 // Implement the launch sequence function here and export it as the default export.
-
 export default function launchSequence() {
   loadPayload(NFSAT);
   loadPayload(FISHSAT);
