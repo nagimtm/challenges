9546171689ae5d0f199348869f9599207b1d2ea8
nagim <a.nagim@protonmail.com>
2023-09-04 12:23:25 +0200

    Finding R2-D2 eye color

--- javascript/js-fetch/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,23 @@
 console.clear();
+import { renderElement } from "./utils.js";
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const res = await fetch(url);
+
+    const data = await res.json();
+    const results = data.results;
+    for (let result of results) {
+      if (result.name == "R2-D2") {
+        console.log(result.eye_color);
+      }
+    }
+  } catch {
+    const errMesage = "Something went wrong!";
+    console.log(errMesage);
+  }
+}
 
 fetchData();
