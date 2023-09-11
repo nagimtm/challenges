6795a1928d1b4ba40192f8a2e8a40282f7f7ab4f
nagim <a.nagim@protonmail.com>
2023-09-11 15:49:45 +0200

    Add Greeting component and its props

--- React/React-Props/react-props_greeting/src/App.js
+++ React/React-Props/react-props_greeting/src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
+import { Greeting } from "./component/Greeting";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="@Eeny" />;
 }
