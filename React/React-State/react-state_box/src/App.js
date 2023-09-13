7e7f547a28ed143cd9a2e08abfcd25e99def39ed
nagim <a.nagim@protonmail.com>
2023-09-13 10:53:01 +0200

    Add useState function, use it in handleClick

--- React/React-State/react-state_box/src/App.js
+++ React/React-State/react-state_box/src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  let [isActive, setActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    isActive = setActive(!isActive);
     // Check that the value changes correctly.
     console.log(isActive);
   }
@@ -12,7 +13,9 @@ export default function App() {
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>
+        {isActive ? "Activate" : "Deactivate"}
+      </button>
     </main>
   );
 }
