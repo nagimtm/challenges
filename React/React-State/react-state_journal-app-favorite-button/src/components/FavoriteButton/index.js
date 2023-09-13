78bae511a24baa9b8498be10b2c935a10a59152d
nagim <a.nagim@protonmail.com>
2023-09-13 15:30:48 +0200

    Add fruits array, map it and set value of Card component

--- React/React-State/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
+++ React/React-State/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
@@ -1,10 +1,11 @@
 import "./FavoriteButton.css";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
+import { useState } from "react";
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
+  const [isFavorite, setFavorite] = useState(false);
 
   return (
     <button
--- React/React-with-Array/react-with-arrays_fruits/src/App.js
+++ React/React-with-Array/react-with-arrays_fruits/src/App.js
@@ -1,11 +1,44 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      name: "Persimmon",
+      id: 52,
+      family: "Ebenaceae",
+      color: "orange",
+    },
+    {
+      name: "Strawberry",
+      id: 3,
+      family: "Rosaceae",
+      color: "red",
+    },
+    {
+      name: "Banana",
+      id: 1,
+      family: "Musaceae",
+      color: "yellow",
+    },
+    {
+      name: "Tomato",
+      id: 5,
+      family: "Solanaceae",
+      color: "red",
+    },
+    {
+      name: "Pear",
+      id: 4,
+      family: "Rosaceae",
+      color: "green",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => {
+        return <Card key={fruit.id} name={fruit.name} color={fruit.color} />;
+      })}
     </div>
   );
 }
