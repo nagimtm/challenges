49213dc26b975ac14722c73d6d1ef3db78980ffe
nagim <a.nagim@protonmail.com>
2023-09-14 15:18:16 +0200

    Add handleSubmit function and share it as a props

--- React/React-State-2/react-state-2_personal-details/src/App.js
+++ React/React-State-2/react-state-2_personal-details/src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form.js";
 
 export default function App() {
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setName(event.target.elements.name.value);
+    setEmail(event.target.elements.email.value);
+    event.target.reset();
+  }
   return (
     <div className="container">
       <h1>Personal Details Form</h1>
-      <Form />
+      <Form onSubmit={handleSubmit} />
       <h2>Your submitted details:</h2>
       <p>
-        Name: <span className="output">John doe</span>
+        Name: <span className="output">{name}</span>
       </p>
       <p>
-        Email: <span className="output">John@Doe.com</span>
+        Email: <span className="output">{email}</span>
       </p>
     </div>
   );
