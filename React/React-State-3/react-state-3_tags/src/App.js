a6c4a6c7909e979fcde19a05b1fd3ea4370a92db
@nagim <91899769+nagimtm@users.noreply.github.com>
2023-09-15 14:03:03 +0200

    Update React/React-State-3/react-state-3_tags/src/App.js
    Co-authored-by: reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>

--- React/React-State-3/react-state-3_tags/src/App.js
+++ React/React-State-3/react-state-3_tags/src/App.js
@@ -11,7 +11,6 @@ export default function App() {
   }
 
   function handleDelete(toDeleteTag) {
-    console.log("I am deleting", toDeleteTag);
     setTags(tags.filter((tag) => tag !== toDeleteTag));
   }
 
