14cce668ab14e74d557a76d761619f357d83778c
nagim <a.nagim@protonmail.com>
2023-09-15 13:08:18 +0200

    Finalizing tags challenge

--- React/React-State-3/react-state-3_tags/src/App.js
+++ React/React-State-3/react-state-3_tags/src/App.js
@@ -6,10 +6,19 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  function handleDelete(toDeleteTag) {
+    console.log("I am deleting", toDeleteTag);
+    setTags(tags.filter((tag) => tag !== toDeleteTag));
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDelete} />
     </main>
   );
 }
