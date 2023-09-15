13ab569713da1ed3de27e2d7feb2932cdb9cf707
nagim <a.nagim@protonmail.com>
2023-09-15 16:10:43 +0200

    Finilyzing pokemon api with paging

--- React/React-Effect-and-Fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ React/React-Effect-and-Fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0);
+  // console.log(pokemon);
 
   useEffect(() => {
-    async function loadPokemon() {
+    async function loadPokemon(page) {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${page}`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -16,13 +18,29 @@ export default function PokemonList() {
       }
     }
 
-    loadPokemon();
-  }, []);
+    loadPokemon(page);
+  }, [page]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button
+        type="button"
+        onClick={() => {
+          setPage(page ? page - 1 : 0);
+          // console.log(page);
+        }}
+      >
+        Previous Page
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setPage(page + 1);
+          // console.log(page);
+        }}
+      >
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
