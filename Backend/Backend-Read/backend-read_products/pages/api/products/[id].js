9e610655c32a789722ff0818fcce0317b650a88f
nagim <a.nagim@protonmail.com>
2023-10-09 12:31:36 +0200

    backend products challenge done

--- Backend/Backend-Read/backend-read_products/pages/api/products/[id].js
+++ Backend/Backend-Read/backend-read_products/pages/api/products/[id].js
@@ -1,13 +1,20 @@
-import { products } from "../../../lib/products";
+// import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect.js";
+import Product from "../../../db/models/Product.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
+  console.log("id", id);
 
-  const product = products.find((product) => product.id === id);
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+    console.log(product);
 
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(product);
   }
-
-  response.status(200).json(product);
 }
